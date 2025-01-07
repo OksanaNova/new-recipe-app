befe8f80765285d297d2c8e881494f0b9314c4d3
OksanaNova <o.nova0804@gmail.com>
2025-01-07 10:41:41 -0700

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import './App.css'
 import Benefits from './Benefits';
 import RecipeResults from './RecipeResults';
 
-
-    // 'https://api.edamam.com/api/recipes/v2?type=public&q=salmon&app_id=28795faa&app_key=b0f423e5e26748cb2364b8517419fb1e'
-
 function App() {
 
   const MY_ID = '28795faa';
@@ -14,29 +11,29 @@ function App() {
 
   const [userSearch, setUserSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
-
+  const [wordSubmitted, setWordSubmitted] = useState("potato");
 
   useEffect(() => {
     const getRecipe = async () => {
-      const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=kale&app_id=${MY_ID}&app_key=${MY_KEY}`);
+      const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${MY_ID}&app_key=${MY_KEY}`);
       const data = await response.json();
-      console.log(data.hits)
       setSearchResult(data.hits)
     }
     getRecipe()
-  }, [])
+  }, [wordSubmitted])
 
   const myRecipeSearch = (e) => {
-    // console.log(e.target.value)
     setUserSearch(e.target.value)
   }
 
+  const finalSearch = (e) => {
+    e.preventDefault();
+    setWordSubmitted(userSearch);
+  }
 
   return (
     <div>
-
       <div className='header'>
-        
           <video autoPlay muted loop>
             <source src={myVideo} type='video/mp4'/>
           </video>
@@ -44,27 +41,19 @@ function App() {
           <div className='header_content'>
             <h1>Recipe Search</h1>
             <p className='header'>Discover mouthwatering recipes with just a few clicks</p>
-            <button className='btnStart'>Get Cooking!</button>
           </div>
-
       </div>
 
       <Benefits />
 
       <div className='search_container'>
-        <form>
+        <form onSubmit={finalSearch}>
           <input onChange={myRecipeSearch} value={userSearch} placeholder='Type your ingridients..'/>
           <button className='btnSearch'>SEARCH</button>
         </form>
       </div>
 
       <RecipeResults myRecipeSearchResults={searchResult}/>
-
-
-
-
-
-
 
     </div>
   )
--- src/RecipeResults.jsx
+++ src/RecipeResults.jsx
@@ -8,31 +8,39 @@ function RecipeResults({myRecipeSearchResults}) {
                 const ingredients = item.recipe.ingredientLines;
                 const cuisineType = item.recipe.cuisineType;
                 const url = item.recipe.url;
+                const protein = item.recipe.totalNutrients.PROCNT.quantity;
+                const fat = item.recipe.totalNutrients.FAT.quantity;
+                const carbs = item.recipe.totalNutrients.CHOCDF.quantity;
                 return (
-                    <div key={index}>
-                        <div>
+                    <div key={index} className="recipe_container">
+                        <div className="recipe_container_content">
                             <h3>{label}, {cuisineType} cuisine</h3>
+
                             <h5>{calories.toFixed()} calories</h5>
+
+                            <div className="nutrients">
+                                <p className="nutrient_item one">Protein: {protein.toFixed()}g</p>
+                                <p className="nutrient_item one">Fat: {fat.toFixed()}g</p>
+                                <p className="one">Carbohydrates: {carbs.toFixed()}g</p>
+                            </div>
+
                             <ul>
                                 {ingredients.map((item, index) => (
                                         <li key={index}>{item}</li>
                                 ))}
                             </ul>
-                            <p>
+                            <button className="btnViewRecipe">
                               <a href={url}>View Recipe</a>
-                            </p>
+                            </button>
 
                         </div>
 
-                        <div>
-                            <img src={image} alt="food"/>
+                        <div className="recipe_container_image">
+                            <img className="food_pic" src={image} alt="food"/>
                         </div>
-
-
                     </div>
                 )
             })}
-
         </div>
     )
 }
